34a2fb76e39784f60f3eb4b568a2aafc4b8469e3
Kevin Chang <changk2@oregonstate.edu>
2025-11-30 21:04:48 -0800

    getting rdy for experiments

--- panda_ros2_moveit2/scripts/control_arm.py
+++ panda_ros2_moveit2/scripts/control_arm.py
@@ -6,6 +6,7 @@ import time
 
 # Imports for Cartesian & Gripper Control
 from ros2_data.action import MoveXYZW, MoveXYZ, MoveG
+from ros2_grasping.action import Attacher
 
 from gazebo_msgs.srv import GetEntityState
 
@@ -17,13 +18,15 @@ class CartesianPanda(Node):
         self.xyzw_topic = '/MoveXYZW'
         self.xyz_topic = '/MoveXYZ'
         self.gripper_topic = '/MoveG'
+        self.attach_topic = '/Attacher'
         
         # Clients
         self.get_logger().info('Initializing Cartesian Clients...')
         self._xyzw_client = ActionClient(self, MoveXYZW, self.xyzw_topic)
         self._xyz_client = ActionClient(self, MoveXYZ, self.xyz_topic)
         self._gripper_client = ActionClient(self, MoveG, self.gripper_topic)
-        
+        self._attacher_client = ActionClient(self, Attacher, self.attach_topic)
+
         self.get_logger().info('Waiting for Action Servers...')
         self._xyzw_client.wait_for_server()
         self._xyz_client.wait_for_server()
@@ -38,7 +41,7 @@ class CartesianPanda(Node):
         self._box_state_req.name = 'box'
         self._box_state_req.reference_frame = 'world'
 
-        self._planning_upper_bound = 5
+        self._planning_upper_bound = 4.5
 
     def get_box_state(self):
         future = self._box_state_client.call_async(self._box_state_req)
@@ -53,7 +56,7 @@ class CartesianPanda(Node):
         Moves the End Effector to a specific X,Y,Z coordinate (Meters)
         and Orientation (Degrees).
         """
-        self.get_logger().info(f'\n>>> MOVING TO: X={x:.2f}, Y={y:.2f}, Z={z:.2f} | Pitch={pitch:.1f}')
+        self.get_logger().info("Moving to XYZW!")
         
         goal_msg = MoveXYZW.Goal()
         
@@ -71,32 +74,32 @@ class CartesianPanda(Node):
         rclpy.spin_until_future_complete(self, send_future, timeout_sec=5.0)
         
         if not send_future.done():
-            self.get_logger().error('Goal Send Timed Out!')
+            self.get_logger().error('[XYZW] Goal Send Timed Out!')
             return False
             
         goal_handle = send_future.result()
         if not goal_handle.accepted:
-            self.get_logger().error('Goal Rejected!')
+            self.get_logger().error('[XYZW] Goal Rejected!')
             return False
             
-        self.get_logger().info('Goal Accepted. Moving...')
+        self.get_logger().info('[XYZW] Goal Accepted. Moving...')
         
         # Wait for Result
         res_future = goal_handle.get_result_async()
         rclpy.spin_until_future_complete(self, res_future, timeout_sec=15.0)
         
         if res_future.done():
-            self.get_logger().info('Movement Complete.')
+            self.get_logger().info('[XYZW] Movement Complete.')
             return True
         else:
-            self.get_logger().error('Movement Timed Out!')
+            self.get_logger().error('[XYZW] Movement Timed Out!')
             return False
 
     def move_to_xyz(self, x, y, z, speed=1.0):
         """
         Moves the End Effector to a specific X,Y,Z coordinate (Meters).
         """
-        self.get_logger().info(f'\n>>> MOVING TO: X={x:.2f}, Y={y:.2f}, Z={z:.2f}')
+        self.get_logger().info("Moving to XYZ")
         
         goal_msg = MoveXYZ.Goal()
         
@@ -111,26 +114,57 @@ class CartesianPanda(Node):
         rclpy.spin_until_future_complete(self, send_future, timeout_sec=5.0)
         
         if not send_future.done():
-            self.get_logger().error('Goal Send Timed Out!')
+            self.get_logger().error('[XYZ] Goal Send Timed Out!')
             return False
             
         goal_handle = send_future.result()
         if not goal_handle.accepted:
-            self.get_logger().error('Goal Rejected!')
+            self.get_logger().error('[XYZ] Goal Rejected!')
             return False
             
-        self.get_logger().info('Goal Accepted. Moving...')
+        self.get_logger().info('[XYZ] Goal Accepted. Moving...')
         
         # Wait for Result
         res_future = goal_handle.get_result_async()
         rclpy.spin_until_future_complete(self, res_future, timeout_sec=15.0)
         
         if res_future.done():
-            self.get_logger().info('Movement Complete.')
+            self.get_logger().info('[XYZ] Movement Complete.')
             return True
         else:
-            self.get_logger().error('Movement Timed Out!')
+            self.get_logger().error('[XYZ] Movement Timed Out!')
             return False
+
+    def attach(self):
+        self.get_logger().info("Attaching")
+        msg = Attacher.Goal()
+        msg.object = 'box'
+        msg.endeffector = 'panda_leftfinger'
+
+        send_future = self._attacher_client.send_goal_async(msg)
+        # rclpy.spin_until_future_complete(self, send_future, timeout_sec=5.0)
+        
+        # if not send_future.done():
+        #     self.get_logger().error('[Attach] Goal Send Timed Out!')
+        #     return False
+            
+        # goal_handle = send_future.result()
+        # if not goal_handle.accepted:
+        #     self.get_logger().error('[Attach] Goal Rejected!')
+        #     return False
+            
+        # self.get_logger().info('[Attach] Goal Accepted. Moving...')
+        
+        # Wait for Result
+        # res_future = goal_handle.get_result_async()
+        # rclpy.spin_until_future_complete(self, res_future, timeout_sec=15.0)
+        
+        # if res_future.done():
+        #     self.get_logger().info('[Attach] Movement Complete.')
+        #     return True
+        # else:
+        #     self.get_logger().error('[Attach] Movement Timed Out!')
+        #     return False
 
     def grip(self, width=0.0):
         """
@@ -139,7 +173,7 @@ class CartesianPanda(Node):
                0.0  = Closed
                0.08 = Open (Max width for Panda is ~8cm)
         """
-        self.get_logger().info(f'Gripper Width: {width}')
+        self.get_logger().info(f'Increasing Gripper Width to {width}')
         msg = MoveG.Goal()
         msg.goal = float(width) # FIX: Send float, not string
         
@@ -147,25 +181,25 @@ class CartesianPanda(Node):
         rclpy.spin_until_future_complete(self, send_future, timeout_sec=5.0)
         
         if not send_future.done():
-            self.get_logger().error('Goal Send Timed Out!')
+            self.get_logger().error('[Grip] Goal Send Timed Out!')
             return False
             
         goal_handle = send_future.result()
         if not goal_handle.accepted:
-            self.get_logger().error('Goal Rejected!')
+            self.get_logger().error('[Grip] Goal Rejected!')
             return False
             
-        self.get_logger().info('Goal Accepted. Moving...')
+        self.get_logger().info('[Grip] Goal Accepted. Moving...')
         
         # Wait for Result
         res_future = goal_handle.get_result_async()
         rclpy.spin_until_future_complete(self, res_future, timeout_sec=15.0)
         
         if res_future.done():
-            self.get_logger().info('Movement Complete.')
+            self.get_logger().info('[Grip] Movement Complete.')
             return True
         else:
-            self.get_logger().error('Movement Timed Out!')
+            self.get_logger().error('[Grip] Movement Timed Out!')
             return False
 
 def main(args=None):
@@ -175,35 +209,47 @@ def main(args=None):
     print("Launched node")
 
     try:
-        # 1. Open Hand (0.08m)
-        node.grip(0.04)
+        while True:
+            # 2. Ready Position (High up)
+            # X=0.5, Z=0.6, Pitch=180 (Pointing straight down)
+            node.move_to_xyzw(0.33, 0.0, 0.76 + 0.5, 45, 180, 0)
 
-        # 2. Ready Position (High up)
-        # X=0.5, Z=0.6, Pitch=180 (Pointing straight down)
-        node.move_to_xyzw(0.3, 0.0, 1.2, 45, 180, 0)
-
-        for height in (0.2, 0.1):
+            # Wait until box is back in grasping zone again
             (x, y, z), (vx, vy, vz) = node.get_box_state()
-            node.get_logger().info(f"Got box state: {((x, y, z), (vx, vy, vz))}")
-            predicted_x = x
-            predicted_y = y + vy * (node._planning_upper_bound)
-            predicted_z = z
-            node.get_logger().info(f"Predicted box pose: {(predicted_x, predicted_y, predicted_z)}")
+            while y < -0.5 or z > 0.76 + 0.2 or y > -0.3 or (x == 0 and y == 0 and z == 0):
+                (x, y, z), (vx, vy, vz) = node.get_box_state()
 
-            node.move_to_xyz(predicted_x, predicted_y, predicted_z + height)
+                time.sleep(0.1)
 
-        # node.grip(0.0)
+            st = time.time()
 
-        # 3. Grasp Position (Low, near conveyor)
-        # Z=0.25 is typically a safe grasp height for objects on the belt
-        # node.move_to_pose(0.5, 0.0, 0.25, 0.0, 180.0, 0.0)
-        # time.sleep(0.5)
+            # 1. Open Hand
+            node.grip(0.04)
 
-        # 4. Close Hand (0.0m)
-        # node.grip(0.0)
+            # Multi-step Predictive IK
+            for height in (0.3, 0.1):
+                (x, y, z), (vx, vy, vz) = node.get_box_state()
+                # node.get_logger().info(f"Got box state: {((x, y, z), (vx, vy, vz))}")
+                predicted_x = x
+                predicted_y = y + vy * (node._planning_upper_bound)
+                predicted_z = z
+                # node.get_logger().info(f"Predicted box pose: {(predicted_x, predicted_y, predicted_z)}")
 
-        # 5. Lift Object
-        # node.move_to_pose(0.5, 0.0, 0.6, 0.0, 180.0, 0.0)
+                node.move_to_xyzw(predicted_x, predicted_y, predicted_z + height, 45, 180, 0)
+
+            # Wait for the cube to pass
+            while True:
+                (x, y, z), _ = node.get_box_state()
+                if abs(y - predicted_y) <= 0.01:
+                    node.grip(0.025)
+                    node.attach()
+
+                    node.move_to_xyzw(0.33, 0.0, 0.76 + 0.5, 45, 180, 0)
+                    break
+
+            grasping_time = time.time() - st
+            node.get_logger().info(f"Completed grasp in {grasping_time} seconds")
+
     except KeyboardInterrupt:
         pass
     finally:
--- spawn_object.bash
+++ spawn_object.bash
@@ -2,12 +2,16 @@
 source /opt/ros/humble/setup.bash
 source ~/dev_ws/install/setup.bash
 
-ros2 service call /delete_entity gazebo_msgs/srv/DeleteEntity "{name: 'box'}"
+timeout 5 ros2 service call /delete_entity gazebo_msgs/srv/DeleteEntity "{name: 'box'}"
 sleep 5
+
+xs=("a" "b" "c")
+powers=("1" "2" "3")
+
 while true; do
-    timeout 5 ros2 service call /CONVEYORPOWER conveyorbelt_msgs/srv/ConveyorBeltControl "{power: 2.0}"
+    timeout 5 ros2 service call /CONVEYORPOWER conveyorbelt_msgs/srv/ConveyorBeltControl "{power: 10.0}"
     timeout 5 ros2 run ros2_conveyorbelt SpawnObject.py --package "conveyorbelt_gazebo" --urdf "box.urdf" --name "box" --x 0.33 --y -0.5 --z 0.76
-    sleep 40
+    sleep 25
     timeout 5 ros2 service call /delete_entity gazebo_msgs/srv/DeleteEntity "{name: 'box'}"
-    sleep 5
+    sleep 2
 done
